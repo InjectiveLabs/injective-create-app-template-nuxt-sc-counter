bde9c9c84ff3b940b2f621a971d47d8fb2604293
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2025-03-20 12:43:58 +0100

    chore: minor

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -5,17 +5,17 @@ export const IS_PRODUCTION: boolean = import.meta.env.PROD as boolean;
 export const IS_DEVELOPMENT: boolean = !IS_PRODUCTION;
 
 const env = {
-  VITE_ALCHEMY_GOERLI_KEY: import.meta.env.VITE_ALCHEMY_GOERLI_KEY,
+  VITE_ALCHEMY_SEPOLIA_KEY: import.meta.env.VITE_ALCHEMY_SEPOLIA_KEY,
   VITE_ALCHEMY_KEY: import.meta.env.VITE_ALCHEMY_KEY,
   VITE_NETWORK: import.meta.env.VITE_NETWORK,
   VITE_ETHEREUM_CHAIN_ID: import.meta.env.VITE_ETHEREUM_CHAIN_ID,
   VITE_CHAIN_ID: import.meta.env.VITE_CHAIN_ID,
 };
 
-export const ALCHEMY_GOERLI_KEY = env.VITE_ALCHEMY_GOERLI_KEY;
+export const ALCHEMY_SEPOLIA_KEY = env.VITE_ALCHEMY_SEPOLIA_KEY;
 
-export const alchemyRpcEndpoint = `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_GOERLI_KEY}`;
-export const alchemyWsRpcEndpoint = `wss://eth-goerli.ws.alchemyapi.io/v2/${ALCHEMY_GOERLI_KEY}`;
+export const alchemyRpcEndpoint = `https://eth-sepolia.alchemyapi.io/v2/${ALCHEMY_SEPOLIA_KEY}`;
+export const alchemyWsRpcEndpoint = `wss://eth-sepolia.ws.alchemyapi.io/v2/${ALCHEMY_SEPOLIA_KEY}`;
 
 export const ETHEREUM_CHAIN_ID = (env.VITE_ETHEREUM_CHAIN_ID ||
   EthereumChainId.Sepolia) as EthereumChainId;
